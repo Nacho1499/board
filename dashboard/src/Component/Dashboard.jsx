d1d78c3e68a6f7c1f4686b0b05f24ff9bf3cd337
emmanuel iheanacho <emmanueliheanacho04@gmail.com>
2025-04-21 12:21:56 -0700

    second commit

--- dashboard/src/Component/Dashboard.jsx
+++ dashboard/src/Component/Dashboard.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Chart as chartJS } from "chart.js/auto";
-import { Bar, Doughnut,Line } from "react-chartjs-2";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 import { Container, Row, Col, Card } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import Navbar from "../Component/Navbar.jsx";
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 import { data } from "react-router-dom";
 
 const Dashboard = () => {
-  
   return (
     <>
       <Navbar />
@@ -24,14 +23,16 @@ const Dashboard = () => {
                   You have done 68% 😎 more sales today. Check your new badge in
                   your profile.
                 </Card.Text>
-                <Button variant="danger" className="shadow">View profile</Button>
+                <Button variant="danger" className="shadow">
+                  View profile
+                </Button>
               </Card.Body>
             </Card>
           </Col>
           <Col xs={6} md={3}>
             <Card className="shadow p-3">
               <Card.Body>
-              <i className="fa-solid fa-cart-plus fa-2x text-danger"></i>
+                <i className="fa-solid fa-cart-plus fa-2x text-danger"></i>
                 <Card.Title>Orders</Card.Title>
                 <Card.Text>1,200</Card.Text>
               </Card.Body>
@@ -40,14 +41,13 @@ const Dashboard = () => {
           <Col xs={6} md={3}>
             <Card className="shadow p-3">
               <Card.Body>
-              <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="danger" />
                 <Card.Title>Total sales</Card.Title>
                 <Card.Text>$38.5k</Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
-        
       </Container>
 
       <Container className="mt-3 p-3 mb-">
@@ -57,21 +57,22 @@ const Dashboard = () => {
               <Card.Body>
                 <Card.Title>Revenue</Card.Title>
                 <Card.Text>
-                 <Bar data={{
-                  labels:["A", "B", "C"],
-                  datasets:[
-                    {
-                       label: 'Revenue',
-                       data:[200,300,400]
-                    },
-                    {
-                      label:"loss",
-                      data:[90,80,70]
-                    }
-                  ],
-                 }}/>
+                  <Bar
+                    data={{
+                      labels: ["A", "B", "C"],
+                      datasets: [
+                        {
+                          label: "Revenue",
+                          data: [200, 300, 400],
+                        },
+                        {
+                          label: "loss",
+                          data: [90, 80, 70],
+                        },
+                      ],
+                    }}
+                  />
                 </Card.Text>
-                
               </Card.Body>
             </Card>
           </Col>
@@ -80,21 +81,20 @@ const Dashboard = () => {
               <Card.Body>
                 <Card.Title>Orders</Card.Title>
                 <Card.Text>
-                  <Doughnut 
-                  data={{
-                    labels:["A", "B", "C"],
-                    datasets:[
-                      {
-                         label: 'Revenue',
-                         data:[200,300,400]
-                      },
-                      {
-                        label:"loss",
-                        data:[90,80,70]
-                      }
-                    ],
-                  }}
-                  
+                  <Doughnut
+                    data={{
+                      labels: ["A", "B", "C"],
+                      datasets: [
+                        {
+                          label: "Revenue",
+                          data: [200, 300, 400],
+                        },
+                        {
+                          label: "loss",
+                          data: [90, 80, 70],
+                        },
+                      ],
+                    }}
                   />
                 </Card.Text>
               </Card.Body>
@@ -106,26 +106,25 @@ const Dashboard = () => {
                 <Card.Title>Total sales</Card.Title>
                 <Card.Text>
                   <Line
-                  data={{
-                    labels:["A", "B", "C"],
-                    datasets:[
-                      {
-                         label: 'Revenue',
-                         data:[200,300,400]
-                      },
-                      {
-                        label:"loss",
-                        data:[90,80,70]
-                      }
-                    ],
-                  }}
+                    data={{
+                      labels: ["A", "B", "C"],
+                      datasets: [
+                        {
+                          label: "Revenue",
+                          data: [200, 300, 400],
+                        },
+                        {
+                          label: "loss",
+                          data: [90, 80, 70],
+                        },
+                      ],
+                    }}
                   />
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
-        
       </Container>
     </>
   );
